391459e3a218917f88ce217aa3f0c929d79053c7
Marcos <marcaosi2014@gmail.com>
2020-06-08 19:21:49 -0300

    Refactor to Header Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import React, { useState } from 'react';
-import { Navbar, NavbarToggler, Collapse, Nav, NavItem, NavLink } from 'reactstrap';
+import React from 'react';
+import Header from './Components/Header'
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false)
   return (
     <>
-      <Navbar expand="md" color="dark" dark>
-        <NavbarToggler onClick={() => setIsOpen(!isOpen)} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav navbar className="m-auto">
-            <NavItem>
-              <NavLink href="/livros/cadastrar">Adicionar Livros</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/livros">Listar Livros</NavLink>
-            </NavItem>
-          </Nav>
-        </Collapse>
-      </Navbar>
+      <Header />
     </>
   );
 }
---
+++ src/Components/Header/index.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import { Navbar, NavbarToggler, Collapse, Nav, NavItem, NavLink } from 'reactstrap';
+
+export default () => {
+    const [isOpen, setIsOpen] = useState(false)
+    return (
+        <Navbar expand="md" color="dark" dark>
+            <NavbarToggler onClick={() => setIsOpen(!isOpen)} />
+            <Collapse isOpen={isOpen} navbar>
+            <Nav navbar className="m-auto">
+                <NavItem>
+                    <NavLink href="/livros/cadastrar">Adicionar Livros</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink href="/livros">Listar Livros</NavLink>
+                </NavItem>
+            </Nav>
+            </Collapse>
+        </Navbar>
+    )
+}
